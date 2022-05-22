e04c807c9e57685ce25855504a675275288c4b87
zhewei chen <38755662+czwzsf@users.noreply.github.com>
2022-05-23 07:53:46 +0800

    22/5/23/02-没有部门列表

--- project2/urls.py
+++ project2/urls.py
@@ -21,11 +21,11 @@ from app01 import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # 部门管理
-    path('depart/list/', views.depart_list),
-    path('servicestaion/', views.servicestation_list),
-    path('depart/add/', views.depart_add),
-    path('depart/delete/', views.depart_delete),  # 就算没有编辑网络页面，也需要在urls里面注册网址
-    path('depart/<int:nid>/edit/', views.depart_edit),
+    # path('depart/list/', views.depart_list),
+    # path('servicestaion/', views.servicestation_list),
+    # path('depart/add/', views.depart_add),
+    # path('depart/delete/', views.depart_delete),  # 就算没有编辑网络页面，也需要在urls里面注册网址
+    # path('depart/<int:nid>/edit/', views.depart_edit),
     # 用户管理
     path('user/list/', views.user_list),
     path('user/add/', views.user_add),
