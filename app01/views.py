3e58ef2f064288f3418df79afebfdcdecaa27271
zhewei chen <38755662+czwzsf@users.noreply.github.com>
2022-05-23 08:50:51 +0800

    22/5/23/04

--- app01/views.py
+++ app01/views.py
@@ -175,7 +175,8 @@ def login(request):
             return render(request, 'html/User/user_login.html', {'form': form})
         # 用户名与密码正确
         # 用户生成随机字符串；写到用户浏览器的cookie中去
-        request.session["info"] = user_object.name
+        # 将cookies写入
+        request.session["info"] = {'id': user_object.id, 'name': user_object.name}
         return redirect('/user/list/')
     else:
         return render(request, 'html/User/user_login.html', {'form': form})
