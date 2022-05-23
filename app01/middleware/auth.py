b8a2d98fcc0df60f1007e63208140d71ea92304e
zhewei chen <1241770441@qq.com>
2022-05-23 10:01:54 +0800

    实现了对中间件的第一次运用

---
+++ app01/middleware/auth.py
@@ -0,0 +1,15 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.shortcuts import HttpResponse, redirect
+
+
+class Login_validation(MiddlewareMixin):
+    """实现登入校验的工作"""
+
+    def process_requset(self, request):
+        # 读取当前访问的用户的session信息，如果能读取到，就可以登入
+        if request.path_info == "/login/":  # 将登入界面排除到中间件判定外，免得重复请求登入
+            return None
+        info_dict = request.session.get("info")
+        if info_dict:
+            return None  # 返回None则请求继续向前传递
+        return redirect('/login/')  # 没有登入过的话，回到登入界面
